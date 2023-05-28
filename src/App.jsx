2c68e160a149b958192b270adf2fc8f847faa5d9
Austin Mazur <mazuraustin1@gmail.com>
2023-05-28 17:16:08 +0200

    Finish iteration two

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,33 @@ import { useState } from "react"
 import contactData from "./contacts.json"
 
 function App() {
-  const [contacts, setContacts] = useState(contactData.slice(0,5))
+  const [contacts, setContacts] = useState(contactData.slice(0,7))
   
   return (
-    <div className="m-6">
-      <h1 className="text-center text-6xl text-bold p-6">IronContacts</h1>
-      <table>
-        <thead >
-        <tr className="bg-blue-200">
+    <div className=" flex flex-col items-center justify-center bg-slate-500">
+      <h1 className="text-center text-6xl text-bold p-6 text-white">IronContacts</h1>
+      <table >
+        <thead className="bg-blue-300" >
+        <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th className="px-6 ">Won an Oscar</th>
+            <th className="px-6 ">Won an Emmy</th>
         </tr>
         </thead>
         <tbody>
         {contacts.map((contact) => (
         <tr key={contact.id}>
-            <td>
-              <img className="w-40 p-2" src={contact.pictureUrl} alt={contact.name} />
+            <td className="text-center">
+              <img className="w-40" src={contact.pictureUrl} alt={contact.name} />
             </td>
-              <td className="text-center">{contact.name}</td>
-              <td className="text-center">{contact.popularity}</td>
+              <td className="text-center bg-blue-100 p-6">{contact.name}</td>
+              <td className="text-center bg-blue-200 p-6">{contact.popularity}</td>
+              <td className="text-center bg-blue-100 p-6">{contact.wonOscar && "🏆"}</td>
+              <td className="text-center bg-blue-200 p-6">{contact.wonEmmy && "🏆"}</td>
             </tr>
-          ))};
+          ))}
         </tbody>
       </table>
     </div>
